1bdf2f15d87a853d196c5f13e00fd93bd32abe82
Ashu-tosh-Kr <kr.ashu465@gmail.com>
2021-10-15 13:30:43 +0530

    update header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,13 +1,21 @@
-import { Alert } from "@mui/material";
+import { Alert, CircularProgress } from "@mui/material";
+import { Box } from "@mui/system";
 import { useGetTempleDetails } from "../../api/hooks";
 import "./Header.css";
 function Header({ productId }) {
   const { templeDetails, isLoading, isError } = useGetTempleDetails(productId);
   if (isLoading)
     return (
-      <Alert sx={{ m: 3 }} severity="info">
-        Loading
-      </Alert>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          w: "100%",
+        }}
+      >
+        <CircularProgress />
+      </Box>
     );
   if (isError)
     return (
